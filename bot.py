5e87d7c532a748d6a6516a3ee773596e46c8decd
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-03 19:25:28 +0400

    upd

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 import telebot
 
-bot = telebot.TeleBot(token = "682595564:AAH-nTcnj255cHSbuEXLq09SjzOrwwx4W7Y")
+bot = telebot.TeleBot(token="682595564:AAH-nTcnj255cHSbuEXLq09SjzOrwwx4W7Y")
 
-@bot.message_handler(content_types=("text"))
-
+@bot.message_handler(content_types=['text'])
 def send_message(message):
-		bot.reply_to(message.chat.id, message.text)
+  bot.reply_to(message, message.text)
 
-if __name__ == "__main__":
-		bot.polling(none_stop = True)
+if name == "main":
+  bot.polling(none_stop=True)
 
 
