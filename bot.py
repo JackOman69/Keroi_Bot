bd869f833b4c9362d81ed457edbfbef3ef70e348
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-03 19:30:16 +0400

    Upd

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(token="682595564:AAH-nTcnj255cHSbuEXLq09SjzOrwwx4W7Y")
 def send_message(message):
   bot.reply_to(message, message.text)
 
-if name == "main":
+if __name__ == "__main__":
   bot.polling(none_stop=True)
 
 
