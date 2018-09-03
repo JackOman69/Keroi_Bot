3eafec990e309d06ff5d5753db87784fc0841904
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-03 19:43:19 +0400

    Upd

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@ import telebot
 
 bot = telebot.TeleBot(token="682595564:AAH-nTcnj255cHSbuEXLq09SjzOrwwx4W7Y")
 
-@bot.message_handler(content_types=['text'])
-def send_message(message):
-  bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+		bot.reply_to(message, 'How are you, nigga?')
+
 
 if __name__ == "__main__":
   bot.polling(none_stop=True)
