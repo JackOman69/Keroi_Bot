934652109362a744be9342595bd2af2eda3547af
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-05 17:08:51 +0400

    upd

--- bot.py
+++ bot.py
@@ -2,9 +2,7 @@ import telebot
 
 bot = telebot.TeleBot(token="682595564:AAH-nTcnj255cHSbuEXLq09SjzOrwwx4W7Y")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-		bot.reply_to(message, 'How are you, nigga?')
+bot.send_message(509237723, "Привет!")
 
 
 
