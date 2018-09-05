4f252c57ef3e366c5eecf730c81aeea9902524ba
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-05 17:22:24 +0400

    upd

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import telebot
 import consts
 
-bot = telebot.TeleBot(contst.token)
+bot = telebot.TeleBot(consts.token)
 
 bot.send_message(509237723, "Привет!")
 
 upd = bot.get_updates()
-print(upd)
+
+last_upd = upd[-1]
+message_from_user = last_upd.message
+print(message_from_user)
 
