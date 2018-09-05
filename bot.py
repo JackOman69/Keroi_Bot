bf44791333d1425e8067aae3f6ce5bdabcfa2100
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-05 17:31:40 +0400

    Upd

--- bot.py
+++ bot.py
@@ -3,11 +3,17 @@ import consts
 
 bot = telebot.TeleBot(consts.token)
 
-bot.send_message(509237723, "Привет!")
+# bot.send_message(509237723, "Привет!")
 
-upd = bot.get_updates()
+@bot.message_handler(content_type = "text")
+def handle_text(message):
+		print("Пришло сообщение от пользователя")
 
-last_upd = upd[-1]
-message_from_user = last_upd.message
-print(message_from_user)
+bot.polling(none_stop = True)
+
+
+ #upd = bot.get_updates()
+ #last_upd = upd[-1]
+ #message_from_user = last_upd.message
+ #print(message_from_user)
 
