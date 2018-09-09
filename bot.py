6e48ccbdd292d32e71a4a1257fae94977c94bc29
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-09 11:47:56 +0400

    upd

--- bot.py
+++ bot.py
@@ -5,12 +5,13 @@ bot = telebot.TeleBot(consts.token)
 
 @bot.message_handler(commands = ["start"])
 def handle_start(message):
-		user_markup = telebot.types.ReplyKeyboardMarkup()
+		user_markup = telebot.types.ReplyKeyboardMarkup(True)
 		user_markup.row("/start", "/stop")
 		user_markup.row("фото", "аудио","документы")
 		user_markup.row("стикер", "голос", "видео", "локация")
 		bot.send_message(message.from_user.id, "Добро пожаловать!", reply_markup = user.markup)
 
+bot.polling(none_stop=True)
 # @bot.message_handler(content_types = ["text"])
 # def handle_text(message):
 # 		if message.text == "Привет!":
@@ -20,8 +21,6 @@ def handle_start(message):
 # 		else:
 # 				bot.send_message(message.from_user.id, "Попробуй еще раз!")
 
-if __name__ = "__main__":
-		bot.polling(none_stop=True)
 
 
  #upd = bot.get_updates()
